c66b39116a5689b9e83812c6ecf1d458fa8a3ce5
pochi <pochi@Hina.local>
2012-03-31 18:14:19 +0900

    bug fix

--- createSnippet.js
+++ createSnippet.js
@@ -1,4 +1,4 @@
-function enchantjsSnippet (argument) {/*
+function enchantjsSnippet () {/*
 enchant();
 
 window.onload = function(){
@@ -9,7 +9,7 @@ window.onload = function(){
 	// overwrite loading scene
 	var loadingScene = new Scene();
 	loadingScene.backgroundColor = "white";
-	loadingScene.addEventListener(Event.Event.PROGRESS, function(e){
+	loadingScene.addEventListener(Event.PROGRESS, function(e){
 		var progress = parseInt(e.loaded / e.total);
 	});
 	game.loadingScene = loadingScene;
@@ -26,4 +26,30 @@ window.onload = function(){
 };
 */}
 
-console.log(enchantjsSnippet.toString().replace(/\t/g, "\\t").replace(/\"/g, "\\\"").split(/\n/).slice(1, -1).join("\\n"));
+function htmlSnippet () {/*
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no"> 
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="format-detection" content="telephone=no" />
+	<script type="text/javascript" src="./enchant.js"></script>
+	<script type="text/javascript" src="./main.js"></script>
+	<style type="text/css">
+		body { margin: 0; padding: 0; }
+	</style>
+</head>
+<body>
+</body>
+</html>
+*/}
+
+function formatHereDoc (str) {
+	return str.replace(/\t/g, "\\t").replace(/\"/g, "\\\"").split(/\n/).slice(1, -1).join("\\n");
+}
+
+
+console.log(formatHereDoc(enchantjsSnippet.toString()));
+console.log(formatHereDoc(htmlSnippet.toString()));
